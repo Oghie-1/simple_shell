d1ae9d668b4f33703396bbee1b9bf3e48ee226a4
AfroTyche <aigbemanuel@gmail.com>
2023-05-24 03:56:17 -0700

    added main.c and shell.h files

---
+++ main.c
@@ -0,0 +1,78 @@
+#include "shell.h"
+
+/**
+ * main - A simple shell that executes programs in the system.
+ *
+ * @argc: Num of args from the command line, passed to shell at launch.
+ * @argv: Array of args passed to shell at launch.
+ * @env: Array of env variables.
+ * Return:  0 upon success, or the number given to "exit".
+ */
+
+int main(int argc, char **argv, char **env)
+{
+char **free_path_token = tokenizer(var_finder("PATH", env), ":=");
+/* Tokenizes the "PATH" environmental variable using the delimiter ":=" */
+
+char **path_token = free_path_token + 1;
+
+/* Points to the second element of free_path_token, skipping the variable name */
+
+char *line = NULL;
+char **array = NULL;
+ssize_t go = 0;
+size_t count = 0;
+int exit_code = 0;
+int i = 0;
+(void)argc;
+
+while (42)
+{
+count = 0;
+line = NULL;
+array = NULL;
+i++;
+
+write(STDIN_FILENO, "\033[1;35m$\033[0m ", 13);
+/* Prints the shell prompt "$" in magenta color */
+
+go = getline(&line, &count, stdin);
+/* Reads a line of input from the user */
+
+if (run_shell(go))
+{
+free(line);
+break;
+}
+/* Checks if the shell should exit based on the return value of run_shell */
+
+if (line != NULL && line[0] == '\n')
+{
+free(line);
+line = NULL;
+}
+/* Handles empty lines by freeing the line and setting it to NULL */
+
+if (line != NULL)
+{
+array = tokenizer(line, "\n ");
+free(line);
+}
+/* Tokenizes the line using the delimiter "\n " and frees the line */
+
+if (life(array, argv, env, path_token, i, &exit_code))
+break;
+/* Executes the command and handles shell functionality */
+
+free_array(array);
+/* Frees the array of tokens */
+}
+
+free_array(free_path_token);
+free_array(array);
+/* Frees the arrays used for tokenization */
+
+return exit_code;
+/* Returns the exit code */
+}
+
---
+++ shell.h
@@ -0,0 +1,32 @@
+#ifndef SHELL_HEADER_H
+#define SHELL_HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/* Macro definitions */
+#define win(x) write(STDOUT_FILENO, x, _strlen(x))
+#define din() write(STDOUT_FILENO, "###\n", 4)
+
+/* Function prototypes */
+size_t _strlen(const char *s);
+int _strcmp(const char *s1, const char *s2);
+char **tokenizer(char *str, const char *delim);
+char *smart_cat(char **path, char *name);
+int forking_helper(char **av);
+int print_env(char **env);
+int custom_atoi(int *status, const char *s);
+char *_strdup(const char *str);
+char *_itoa(int num);
+char *var_finder(const char *var, char **env);
+void free_array(char **array);
+int life(char **array, char **argv, char **env, char **p_t, int i, int *e_c);
+int run_shell(int go);
+
+#endif /* SHELL_HEADER_H */
+
