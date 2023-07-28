c8cef44b9678c2cb0bb7b56e27215f6c5b286c9f
afro <aigbemanuel@gmail.com>
2023-07-28 04:53:42 -0700

    added error messages for the display prompt function

--- display_shell_prompt.c
+++ display_shell_prompt.c
@@ -3,16 +3,43 @@
 /* Function to display the shell prompt */
 void displayShellPrompt(void)
 {
+char *username = getenv("USER"); /* Get the value of the "USER" environment variable */
 char *currentWorkingDir = getcwd(NULL, 0); /* Use dynamic memory allocation to store the current working directory */
+
+
+
+if (username != NULL)
+{
+printf("%s", username); /* Display the username */
+}
+
+else
+{
+
+/*  Handle the case when the "USER" environment variable is not set */
+perror("Error: USER environment variable is not set");
+printf("Unknown"); /* Display a fallback username*/
+}
 
 if (currentWorkingDir != NULL)
 {
-printf("%s@%s$ ", getenv("USER"), currentWorkingDir); /* Display the shell prompt with username and current working directory */
+printf("@%s$ ", currentWorkingDir); /*Display the prompt with current working directory */
+
 free(currentWorkingDir); /* Free the dynamically allocated memory for the current working directory */
 }
 else
 {
+
+/* Handle the case when either username or cwd is NULL  */
+if (username == NULL)
+{
+perror("Error getting username");
+}
+
+if (currentWorkingDir == NULL)
+{
 perror("Error getting current working directory");
+}
 printf("afro@shell$ "); /* Display a fallback prompt if there was an error getting the current working directory */
 }
 }
